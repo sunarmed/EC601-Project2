83392deccac815f1b922218a658ead9bb590fe6c
sunarmed <sunarmed@gmail.com>
2021-10-10 17:15:52 -0400

    Collecting tweets text and sentiment score

--- tweet-example.py
+++ tweet-example.py
@@ -6,6 +6,8 @@
 
 import tweepy #https://github.com/tweepy/tweepy
 import json
+from google.cloud import language_v1
+
 
 
 # In[2]:
@@ -74,6 +76,65 @@ def search_tweets(keyword,filename):
     print( "Done")
     file.close()
 
+
+
+
+def sample_analyze_entities(text_content):
+    """
+    Analyzing Entities in a String
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
 
 
 
