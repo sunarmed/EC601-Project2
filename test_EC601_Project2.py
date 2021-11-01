00419f378eaa2704a7f3ba2d5328deed8d299cb1
sunarmed <sunarmed@gmail.com>
2021-11-01 11:35:51 -0400

    More unit tests, final

--- test_EC601_Project2.py
+++ test_EC601_Project2.py
@@ -36,7 +36,7 @@ def test_credential_content():
 
 def test_Twitter_server_response():
 	"""
-	Test whether respond to our search requests
+	Test whether TwitterAPI respond to our search requests
 	"""
 	rslt = False
 	keyword = "Love" #This should be a universal keyword.
@@ -52,16 +52,31 @@ def test_Twitter_server_response():
 
 	assert(rslt)
 
+def test_Google_server_response():
+	"""
+	Test whether google-api respond to our requests
+	"""
+	rslt = False
+	test_text = 'I love you, no matter what happen.'
+	try:
+		score , magd = sample_analyze_sentiment(test_text)
+		rslt = True
+	except:
+		pass
+
+	assert(rslt)
+
+
 def test_Twitter_search_complete():
 	"""
 	Test if tweet searching is completed
 	"""
-	max_tweet_number = 300
+	max_tweet_number = 30
 	test_keyword = "love"
 	test_filename = "tweet-unittest-temp.json"
 	rslt = False
 	try:
-		mean, std = search_tweets( city_names , filename , max_tweet_number)
+		mean, std = search_tweets( test_keyword , test_filename , max_tweet_number)
 		rslt = True
 	except:
 		pass
